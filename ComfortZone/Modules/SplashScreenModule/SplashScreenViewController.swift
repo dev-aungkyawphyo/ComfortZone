7e15010d6a9d039c414247f020d111f822cc282c
Aung Kyaw Phyo <Dev.aungkyawphyo@gmail.com>
2024-07-25 02:48:07 +0630

    initial commit

---
+++ ComfortZone/Modules/SplashScreenModule/SplashScreenViewController.swift
@@ -0,0 +1,26 @@
+//
+//  SplashScreenViewController.swift
+//  ComfortZone
+//
+//  Created by Aung Kyaw Phyo on 25/07/2024.
+//
+
+import UIKit
+import Lottie
+
+class SplashScreenViewController: UIViewController {
+    
+    // MARK: Outlets
+    @IBOutlet weak var logoImageview: UIImageView!
+    @IBOutlet weak var anmimationView: LottieAnimationView!
+
+    // MARK: Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+
+    
+
+}
